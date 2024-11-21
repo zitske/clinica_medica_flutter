653eb3461c1cf972e2d2aa4597e7916c66f2e0da
zitske <eduardomontzitske@gmail.com>
2024-11-21 11:36:32 -0300

    login card

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,51 +1,120 @@
 import 'package:clinica_medica_flutter/screens/registro.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui';
+
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwbCm6f9HTL0gXg7jvMZhJTnpuSTzDQDZQoA&s'), // URL da imagem da internet
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.5),
+                  BlendMode.darken,
+                ),
               ),
             ),
-            SizedBox(height: 16.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              child: Container(
+                color: Colors.black.withOpacity(0.1),
               ),
             ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // Handle login logic here
-              },
-              child: Text('Login'),
+          ),
+          Center(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              elevation: 8.0,
+              margin: const EdgeInsets.all(16.0),
+              child: Container(
+                width: 400.0, // Define o width máximo do card
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'LOGIN',
+                      style: GoogleFonts.anekOdia(
+                          fontSize: 36.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green),
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 45.0, // Altura igual à dos TextFields
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle login logic here
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green, // Cor verde
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          shadowColor: Colors.greenAccent, // Cor do efeito neon
+                          elevation: 20.0, // Intensidade da sombra
+                        ),
+                        child: Text(
+                          'Entrar',
+                          style: TextStyle(
+                            color: Colors.white, // Cor do texto
+                            fontWeight: FontWeight.bold, // Negrito
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegistroScreen()),
+                        );
+                      },
+                      child: Text('Não tem uma conta? Registre-se'),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            SizedBox(height: 16.0),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegistroScreen()),
-                );
-              },
-              child: Text('Não tem uma conta? Registre-se'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
