33e24cd3fbc38dcf4eec8a4cc0fb86eb03d9ff41
zitske <eduardomontzitske@gmail.com>
2024-11-22 14:50:48 -0300

    limpeza de arquivos e inicio do cadastro de paciente

--- android/app/src/main/kotlin/com/example/clinica_medica_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.clinica_medica_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../classes/paciente.dart';
 
 class DashboardScreen extends StatelessWidget {
+  final List<Paciente> pacientes = [
+    Paciente(
+        id: 1,
+        nome: 'João Silva',
+        cpf: '123.456.789-00',
+        restricoes: 'Nenhuma',
+        idSecretaria: 1),
+    Paciente(
+        id: 2,
+        nome: 'Maria Souza',
+        cpf: '987.654.321-00',
+        restricoes: 'Alergia a penicilina',
+        idSecretaria: 2),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,6 +48,50 @@ class DashboardScreen extends StatelessWidget {
                 // Navegar para outra tela
               },
               child: Text('Ir para outra tela'),
+            ),
+            SizedBox(height: 20),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text('Cadastrar Novo Paciente',
+                        style: TextStyle(fontSize: 20)),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Nome'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'CPF'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Restrições'),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Lógica para cadastrar paciente
+                      },
+                      child: Text('Cadastrar'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: pacientes.length,
+                itemBuilder: (context, index) {
+                  final paciente = pacientes[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(paciente.nome),
+                      subtitle: Text(
+                          'CPF: ${paciente.cpf}\nRestrições: ${paciente.restricoes}'),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
